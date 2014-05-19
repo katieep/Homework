0ecd6a9cbae1e1d537752ee82164723215fddf9c
katieep <katie.pollak@gmail.com>
2014-05-18 23:11:01 -0400

    I'm adding more stuff

--- myfile.r
+++ myfile.r
@@ -1,6 +1,5 @@
 # input Stata file
 library(foreign)
-teacher_ratio <- read.dta("/Users/Katie/Desktop/Spring\ 2014/Research\ Methods/Problem\ sets/ps5/programeval2014.dta")
-
-teacher_ratio$age
-hist(mydata$age)
+teacher_ratio <- read.dta("programeval2014.dta")
+?write.csv
+write.csv(teacher_ratio,"programeval2014.csv", row.names=FALSE)
