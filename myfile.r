55131804799fee3b11da6518c481d8a529387314
katieep <katie.pollak@gmail.com>
2014-05-18 23:01:51 -0400

    Adding in final problem set

---
+++ myfile.r
@@ -0,0 +1,6 @@
+# input Stata file
+library(foreign)
+teacher_ratio <- read.dta("/Users/Katie/Desktop/Spring\ 2014/Research\ Methods/Problem\ sets/ps5/programeval2014.dta")
+
+teacher_ratio$age
+hist(mydata$age)
